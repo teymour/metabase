9592554bdb42b5ba73cb62b73e3ea63ce078d936
Anton Kulyk <kuliks.anton@gmail.com>
2022-03-30 00:56:43 +0100

    Remove not used code

--- frontend/src/metabase/nav/containers/MainNavbar/BookmarkList/BookmarkList.tsx
+++ frontend/src/metabase/nav/containers/MainNavbar/BookmarkList/BookmarkList.tsx
@@ -12,7 +12,7 @@ import * as Urls from "metabase/lib/urls";
 
 import { SelectedItem } from "../types";
 import { SidebarHeading } from "../MainNavbar.styled";
-import { BookmarkListRoot, SidebarBookmarkItem } from "./BookmarkList.styled";
+import { SidebarBookmarkItem } from "./BookmarkList.styled";
 
 const mapDispatchToProps = {
   onDeleteBookmark: ({ item_id, type }: Bookmark) =>
